31d224202bb85e4ae0e59f5e5339e128a2c99b5c
zmj <928217176@qq.com>
2017-09-06 17:57:09 +0800

    style为normal颜色显示问题:PRODDEV-5680

--- ext_implement/do_ProgressBar_UIView.m
+++ ext_implement/do_ProgressBar_UIView.m
@@ -61,7 +61,7 @@
     else
     {
         do_UIActivityView *activityView = [[do_UIActivityView alloc] initWithActivityIndicatorStyle:@"large"];
-        activityView.color = [UIColor whiteColor];
+        activityView.color = [UIColor grayColor];
         [activityView startAnimating];
         prograssBarView = activityView;
         self.do_activity = activityView;
